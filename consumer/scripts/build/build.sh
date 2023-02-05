5aa05d88e81450fe0f0b93f76bfcaae167730cf7
chilledornaments <mitch@chilledornaments.com>
2023-02-04 17:53:46 -0700

    clean up build script

--- consumer/scripts/build/build.sh
+++ consumer/scripts/build/build.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+# must be run from the consumer/ directory
+mkdir build
+
+cp app.py ./build
+cp requirements.txt ./build
+
+cd build
+
 pip install -r requirements.txt --no-cache-dir -t ./
 
 zip -r app.zip ./
 
+mv app.zip ../
+
+cd ../
+
+rm -rf build/
