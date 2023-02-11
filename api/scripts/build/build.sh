9e830a1693add949f6210a3141108fb1781c9999
chilledornaments <mitch@chilledornaments.com>
2023-02-10 17:24:11 -0700

    update scripts

--- api/scripts/build/build.sh
+++ api/scripts/build/build.sh
@@ -5,10 +5,12 @@ set -e
 # Script must be run from the api/ dir
 # via bash ./scripts/build/build.sh
 
-python3 -m venv app_venv
-source app_venv/bin/activate
-pip install -r requirements.txt --no-cache-dir
-deactivate
+#python3 -m venv app_venv
+#source app_venv/bin/activate
+#pip install -r requirements.txt --no-cache-dir
+#deactivate
 
 # Zip the venv and the app, include the scripts/runtime dir
-zip -r app.zip app_venv/ app.py scripts/runtime/
+#zip -r app.zip app_venv/ app.py scripts/runtime/
+
+zip -r app.zip app.py requirements.txt scripts/runtime/
--- api/scripts/runtime/run.sh
+++ api/scripts/runtime/run.sh
@@ -11,12 +11,15 @@ if [ -f .env ]; then
 else
   TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
   # TODO this is a hack for local testing, we should pull the queue URL from secrets manager or parameter store
-  QUEUE_URL=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/tags/instance/QUEUE_URL)
+  export QUEUE_URL=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/tags/instance/QUEUE_URL)
+  export AWS_DEFAULT_REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -v http://169.254.169.254/latest/meta-data/placement/region)
 fi
 
 echo "QUEUE_URL = ${QUEUE_URL}"
 
 sudo touch /var/log/app.log && sudo chown ec2-user /var/log/app.log
 
+python3 -m pip install -r requirements.txt --no-cache-dir
+
 # call python app.py and send output to log file and send it to the background
-app_venv/bin/python app.py > /var/log/app.log 2>&1 &
+python3 app.py > /var/log/app.log 2>&1 &
